46f0bc8aa8bf59a974642d7fc060676fae89b4fe
ilvme <ikangjia.cn@outlook.com>
2024-12-26 14:38:36 +0800

    feat: mybatis-plus 配置

---
+++ src/main/java/cn/ikangjia/gwds/infra/config/MyBatisPlusConfig.java
@@ -0,0 +1,20 @@
+package cn.ikangjia.gwds.infra.config;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author kangJia
+ * @email ikangjia.cn@outlook.com
+ * @since 2024/12/26 14:23
+ */
+@Configuration
+public class MyBatisPlusConfig {
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        //interceptor.addInnerInterceptor(new PaginationInnerInterceptor()); 如果有多数据源可以不配具体类型 否则都建议配上具体的DbType
+        return interceptor;
+    }
+}
