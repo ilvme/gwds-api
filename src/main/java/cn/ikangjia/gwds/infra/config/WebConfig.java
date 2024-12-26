99277357c43a384249055561a674af4e81b7c081
ilvme <ikangjia.cn@outlook.com>
2024-12-26 14:38:27 +0800

    feat: 跨域配置

---
+++ src/main/java/cn/ikangjia/gwds/infra/config/WebConfig.java
@@ -0,0 +1,22 @@
+package cn.ikangjia.gwds.infra.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author kangJia
+ * @email ikangjia.cn@outlook.com
+ * @since 2024/12/26 14:20
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowCredentials(true)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .maxAge(3600);
+    }
+}
